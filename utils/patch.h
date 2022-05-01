41165241ea5c3d50fdcde511069f93328168d032
Andre Vaz <andre.vaz1411@gmail.com>
2022-05-01 21:49:49 +0100

    update

--- utils/patch.h
+++ utils/patch.h
@@ -13,10 +13,12 @@ using namespace std;
 class Patch {
     private:
         vector<Point> controlPoints;
-
+        vector<Point> normals;
     public:
         void addPoint(Point);
         vector<Point> getPoints();
+        void addNormals(Point);
+        vector<Point> getNormals();
         vector<Point> tessellate(int level);
 };
 
